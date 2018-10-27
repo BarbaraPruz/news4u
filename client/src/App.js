06d88f252e57d1a5ebc52f66258b53bba78ef886
BarbaraPruz <barbara.prusiewicz@gmail.com>
2018-10-27 12:54:54 -0700

    Home/Root page - conditionally render login form or news container

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,31 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import './App.css';
-import Login from './containers/login'
-import NavBar from './components/navbar'
+import Login from './containers/login';
+import NewsContainer from './containers/news_container';
+import NavBar from './components/navbar';
 
 class App extends Component {
+
   render() {
+    let container = (this.props.userLoggedIn) ? <NewsContainer/> : <Login />
+
     return (
       <div className="App">
-        <NavBar />
+        <NavBar userLoggedIn={this.props.userLoggedIn} />
         <section>
-          <h1>News4U</h1>
-          <Login />
+          {container}
         </section>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    userLoggedIn: state.user.isLoggedIn
+  }
+}
+
+export default connect(mapStateToProps)(App);
---
+++ client/src/containers/news_container.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+//import { connect } from 'react-redux';
+
+
+class NewsContainer extends Component {
+
+    render() {
+        return (
+            <div className="news_container">
+                <h2>News Container</h2>         
+            </div>
+        );
+    }
+}
+
+export default NewsContainer;
