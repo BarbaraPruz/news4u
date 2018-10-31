8fd05ca40f7f6fd6ef57bf0162cd4f359855ee17
BarbaraPruz <barbara.prusiewicz@gmail.com>
2018-10-31 15:35:59 -0700

    About page - add style

--- client/src/components/about.js
+++ client/src/components/about.js
@@ -2,18 +2,16 @@ import React from 'react'
 
 
 const About =  () =>
-    <div id="AboutPage">
-        <section>
+    <div className="content-section">
         <h1>News4u - Customize your news feed!</h1>
-        <h2>A Flatiron Online Bootcamp React/Redux portfolio project</h2>
+        <h2><em>A Flatiron Online Bootcamp React/Redux portfolio project</em></h2>
 
         <p>The News4u app lets users retrieve headlines and stories from different news sources.
         News is retrieved from <a href="https://newsapi.org/" target="_blank" rel="noopener noreferrer">NewsAPI.org</a>.
         News sources available through News4u app are limited to English.
         </p>      
-
-        <p>Stay informed!</p>
-        </section>      
+        <p><a href="https://unsplash.com/@rawpixel" target="_blank" rel="noopener noreferrer">Photo by @rawpixel on Unsplash</a></p>
+        <p>Stay informed!</p>   
     </div>
 ;
 
