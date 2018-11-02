32681da0fed1f30371d11a992800e488f231f1d4
BarbaraPruz <barbara.prusiewicz@gmail.com>
2018-11-01 18:38:44 -0700

    Add filter form to headlines page.

--- client/src/containers/headlines_container.js
+++ client/src/containers/headlines_container.js
@@ -3,14 +3,38 @@ import { connect } from 'react-redux';
 
 import { getHeadlines } from '../actions/news'
 import Headline from '../components/headline'
+import FilterForm from '../containers/filter_form'
 
 class HeadlinesContainer extends Component {
+
+    constructor (props) {
+        super(props);
+        this.state = {
+            selectedSources: props.newsSources,
+        };
+    }  
 
     componentDidMount() {
         //    if logged in, start action to retrieve data for user 
         if (this.props.isLoggedIn) {
             this.props.getHeadlines(this.props.newsSources)
         }
+    }
+
+    onChange = (event) => {
+        let name = event.target.value;
+        let tempSources=this.state.selectedSources;
+        console.log("onchange start",name,tempSources.length,tempSources);
+        console.log("and allsources",this.props.newsSources.length,this.props.newsSources);         
+        let index = tempSources.findIndex( (s)=> s.news_source_id===name);
+        if (index >= 0)
+            tempSources.splice(index,1);
+        else {
+            tempSources.push(this.props.newsSources.find((s) => s.news_source_id===name))
+        }
+        this.setState({selectedSources: tempSources});
+        console.log("onchange end",this.state.selectedSources);
+        console.log("and allsources",this.props.newsSources);        
     }
 
     render() {
@@ -21,6 +45,7 @@ class HeadlinesContainer extends Component {
         return (
             <div className="content-section">
                 <h1>Headlines</h1>
+                <FilterForm onChange={this.onChange} allSources={this.props.newsSources} selectedSources={this.state.selectedSources} />                
                 { this.props.headlines.map((hl, id) => <Headline headline={hl} key={id} /> ) }
             </div> 
         )
