213979ea39a4c92a8d7182449a9a3ce456f863e2
BarbaraPruz <barbara.prusiewicz@gmail.com>
2019-02-01 16:45:19 -0800

    Modify procfile for heroku deployment

--- client/src/components/about.js
+++ client/src/components/about.js
@@ -3,7 +3,7 @@ import React from 'react'
 const About =  () =>
     <div className="content-section">
         <h1>News4u - Customize your news feed!</h1>
-        <h2><em>A Flatiron Online Bootcamp React/Redux portfolio project</em></h2>
+        <h2><em>A Flatiron Online Bootcamp Portfolio Project with Rails and React/Redux</em></h2>
 
         <p>The News4u app lets users retrieve headlines and stories from different news sources.
         News is retrieved from <a href="https://newsapi.org/" target="_blank" rel="noopener noreferrer">NewsAPI.org</a>.
